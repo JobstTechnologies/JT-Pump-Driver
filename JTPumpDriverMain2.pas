JobstTechnologies/JT-Pump-Driver

commit 8ea24e4c553713bbe34cf480c1cc536f2c3d63b4

code simplifications for future support
of more pumps and steps

- also a small bugfix

JTPumpDriver2.lpi
@@ -21,8 +21,8 @@
       <UseVersionInfo Value="True"/>
       <MajorVersionNr Value="2"/>
       <MinorVersionNr Value="5"/>
-      <RevisionNr Value="5"/>
-      <StringTable CompanyName="Jobst Technologies" FileDescription="Program to control pump driver for CCP1 pumps" LegalCopyright="Jobst Technologies GmbH" ProductName="JT Pump Driver" ProductVersion="2.55"/>
+      <RevisionNr Value="6"/>
+      <StringTable CompanyName="Jobst Technologies" FileDescription="Program to control pump driver for CCP1 pumps" LegalCopyright="Jobst Technologies GmbH" ProductName="JT Pump Driver" ProductVersion="2.56"/>
     </VersionInfo>
     <BuildModes Count="3">
       <Item1 Name="Default" Default="True"/>

jtpumpdrivermain2.lfm
@@ -43,7 +43,7 @@ object MainForm: TMainForm
         ParentShowHint = False
         ShowHint = True
         TabOrder = 1
-        OnDblClick = Pump1GBDblClick
+        OnDblClick = PumpGBDblClick
         object Pump1OnOffCB1: TCheckBox
           Left = 8
           Height = 19
@@ -119,7 +119,7 @@ object MainForm: TMainForm
         ParentShowHint = False
         ShowHint = True
         TabOrder = 4
-        OnDblClick = Pump4GBDblClick
+        OnDblClick = PumpGBDblClick
         object Pump4OnOffCB1: TCheckBox
           Left = 8
           Height = 19
@@ -195,7 +195,7 @@ object MainForm: TMainForm
         ParentShowHint = False
         ShowHint = True
         TabOrder = 2
-        OnDblClick = Pump2GBDblClick
+        OnDblClick = PumpGBDblClick
         object Pump2OnOffCB1: TCheckBox
           Left = 8
           Height = 19
@@ -271,7 +271,7 @@ object MainForm: TMainForm
         ParentShowHint = False
         ShowHint = True
         TabOrder = 3
-        OnDblClick = Pump3GBDblClick
+        OnDblClick = PumpGBDblClick
         object Pump3OnOffCB1: TCheckBox
           Left = 8
           Height = 19
@@ -428,7 +428,7 @@ object MainForm: TMainForm
         Width = 39
         Caption = 'Use'
         Checked = True
-        OnChange = Step2UseCBChange
+        OnChange = StepXUseCBChange
         ParentFont = False
         ParentShowHint = False
         State = cbChecked
@@ -448,7 +448,7 @@ object MainForm: TMainForm
         Top = 20
         Width = 39
         Caption = 'Use'
-        OnChange = Step2UseCBChange
+        OnChange = StepXUseCBChange
         ParentFont = False
         ParentShowHint = False
         ShowHint = True
@@ -465,7 +465,7 @@ object MainForm: TMainForm
         Enabled = False
         ParentFont = False
         TabOrder = 4
-        OnDblClick = Pump3GBDblClick
+        OnDblClick = PumpGBDblClick
         object Pump3OnOffCB2: TCheckBox
           Left = 8
           Height = 19
@@ -537,7 +537,7 @@ object MainForm: TMainForm
         Enabled = False
         ParentFont = False
         TabOrder = 3
-        OnDblClick = Pump2GBDblClick
+        OnDblClick = PumpGBDblClick
         object Pump2OnOffCB2: TCheckBox
           Left = 8
           Height = 19
@@ -609,7 +609,7 @@ object MainForm: TMainForm
         Enabled = False
         ParentFont = False
         TabOrder = 5
-        OnDblClick = Pump4GBDblClick
+        OnDblClick = PumpGBDblClick
         object Pump4OnOffCB2: TCheckBox
           Left = 8
           Height = 19
@@ -681,7 +681,7 @@ object MainForm: TMainForm
         Enabled = False
         ParentFont = False
         TabOrder = 2
-        OnDblClick = Pump1GBDblClick
+        OnDblClick = PumpGBDblClick
         object Pump1OnOffCB2: TCheckBox
           Left = 8
           Height = 19
@@ -845,7 +845,7 @@ object MainForm: TMainForm
         Top = 20
         Width = 39
         Caption = 'Use'
-        OnChange = Step3UseCBChange
+        OnChange = StepXUseCBChange
         ParentFont = False
         ParentShowHint = False
         ShowHint = True
@@ -862,7 +862,7 @@ object MainForm: TMainForm
         Enabled = False
         ParentFont = False
         TabOrder = 4
-        OnDblClick = Pump3GBDblClick
+        OnDblClick = PumpGBDblClick
         object Pump3OnOffCB3: TCheckBox
           Left = 8
           Height = 19
@@ -934,7 +934,7 @@ object MainForm: TMainForm
         Enabled = False
         ParentFont = False
         TabOrder = 3
-        OnDblClick = Pump2GBDblClick
+        OnDblClick = PumpGBDblClick
         object Pump2OnOffCB3: TCheckBox
           Left = 8
           Height = 19
@@ -1006,7 +1006,7 @@ object MainForm: TMainForm
         Enabled = False
         ParentFont = False
         TabOrder = 5
-        OnDblClick = Pump4GBDblClick
+        OnDblClick = PumpGBDblClick
         object Pump4OnOffCB3: TCheckBox
           Left = 8
           Height = 19
@@ -1078,7 +1078,7 @@ object MainForm: TMainForm
         Enabled = False
         ParentFont = False
         TabOrder = 2
-        OnDblClick = Pump1GBDblClick
+        OnDblClick = PumpGBDblClick
         object Pump1OnOffCB3: TCheckBox
           Left = 8
           Height = 19
@@ -1242,7 +1242,7 @@ object MainForm: TMainForm
         Top = 20
         Width = 39
         Caption = 'Use'
-        OnChange = Step4UseCBChange
+        OnChange = StepXUseCBChange
         ParentFont = False
         ParentShowHint = False
         ShowHint = True
@@ -1259,7 +1259,7 @@ object MainForm: TMainForm
         Enabled = False
         ParentFont = False
         TabOrder = 4
-        OnDblClick = Pump3GBDblClick
+        OnDblClick = PumpGBDblClick
         object Pump3OnOffCB4: TCheckBox
           Left = 8
           Height = 19
@@ -1331,7 +1331,7 @@ object MainForm: TMainForm
         Enabled = False
         ParentFont = False
         TabOrder = 3
-        OnDblClick = Pump2GBDblClick
+        OnDblClick = PumpGBDblClick
         object Pump2OnOffCB4: TCheckBox
           Left = 8
           Height = 19
@@ -1403,7 +1403,7 @@ object MainForm: TMainForm
         Enabled = False
         ParentFont = False
         TabOrder = 5
-        OnDblClick = Pump4GBDblClick
+        OnDblClick = PumpGBDblClick
         object Pump4OnOffCB4: TCheckBox
           Left = 8
           Height = 19
@@ -1475,7 +1475,7 @@ object MainForm: TMainForm
         Enabled = False
         ParentFont = False
         TabOrder = 2
-        OnDblClick = Pump1GBDblClick
+        OnDblClick = PumpGBDblClick
         object Pump1OnOffCB4: TCheckBox
           Left = 8
           Height = 19
@@ -1639,7 +1639,7 @@ object MainForm: TMainForm
         Top = 20
         Width = 39
         Caption = 'Use'
-        OnChange = Step5UseCBChange
+        OnChange = StepXUseCBChange
         ParentFont = False
         ParentShowHint = False
         ShowHint = True
@@ -1656,7 +1656,7 @@ object MainForm: TMainForm
         Enabled = False
         ParentFont = False
         TabOrder = 4
-        OnDblClick = Pump3GBDblClick
+        OnDblClick = PumpGBDblClick
         object Pump3OnOffCB5: TCheckBox
           Left = 8
           Height = 19
@@ -1728,7 +1728,7 @@ object MainForm: TMainForm
         Enabled = False
         ParentFont = False
         TabOrder = 3
-        OnDblClick = Pump2GBDblClick
+        OnDblClick = PumpGBDblClick
         object Pump2OnOffCB5: TCheckBox
           Left = 8
           Height = 19
@@ -1800,7 +1800,7 @@ object MainForm: TMainForm
         Enabled = False
         ParentFont = False
         TabOrder = 5
-        OnDblClick = Pump4GBDblClick
+        OnDblClick = PumpGBDblClick
         object Pump4OnOffCB5: TCheckBox
           Left = 8
           Height = 19
@@ -1872,7 +1872,7 @@ object MainForm: TMainForm
         Enabled = False
         ParentFont = False
         TabOrder = 2
-        OnDblClick = Pump1GBDblClick
+        OnDblClick = PumpGBDblClick
         object Pump1OnOffCB5: TCheckBox
           Left = 8
           Height = 19
@@ -2036,7 +2036,7 @@ object MainForm: TMainForm
         Top = 20
         Width = 39
         Caption = 'Use'
-        OnChange = Step6UseCBChange
+        OnChange = StepXUseCBChange
         ParentFont = False
         ParentShowHint = False
         ShowHint = True
@@ -2053,7 +2053,7 @@ object MainForm: TMainForm
         Enabled = False
         ParentFont = False
         TabOrder = 4
-        OnDblClick = Pump3GBDblClick
+        OnDblClick = PumpGBDblClick
         object Pump3OnOffCB6: TCheckBox
           Left = 8
           Height = 19
@@ -2125,7 +2125,7 @@ object MainForm: TMainForm
         Enabled = False
         ParentFont = False
         TabOrder = 3
-        OnDblClick = Pump2GBDblClick
+        OnDblClick = PumpGBDblClick
         object Pump2OnOffCB6: TCheckBox
           Left = 8
           Height = 19
@@ -2197,7 +2197,7 @@ object MainForm: TMainForm
         Enabled = False
         ParentFont = False
         TabOrder = 5
-        OnDblClick = Pump4GBDblClick
+        OnDblClick = PumpGBDblClick
         object Pump4OnOffCB6: TCheckBox
           Left = 8
           Height = 19
@@ -2269,7 +2269,7 @@ object MainForm: TMainForm
         Enabled = False
         ParentFont = False
         TabOrder = 2
-        OnDblClick = Pump1GBDblClick
+        OnDblClick = PumpGBDblClick
         object Pump1OnOffCB6: TCheckBox
           Left = 8
           Height = 19
@@ -2519,7 +2519,7 @@ object MainForm: TMainForm
         Top = 20
         Width = 39
         Caption = 'Use'
-        OnChange = Step7UseCBChange
+        OnChange = StepXUseCBChange
         ParentFont = False
         ParentShowHint = False
         ShowHint = True
@@ -2537,7 +2537,7 @@ object MainForm: TMainForm
         ParentColor = False
         ParentFont = False
         TabOrder = 3
-        OnDblClick = Pump1GBDblClick
+        OnDblClick = PumpGBDblClick
         object Pump1OnOffCB7: TCheckBox
           Left = 8
           Height = 19
@@ -2612,7 +2612,7 @@ object MainForm: TMainForm
         Enabled = False
         ParentFont = False
         TabOrder = 4
-        OnDblClick = Pump2GBDblClick
+        OnDblClick = PumpGBDblClick
         object Pump2OnOffCB7: TCheckBox
           Left = 8
           Height = 19
@@ -2686,7 +2686,7 @@ object MainForm: TMainForm
         Enabled = False
         ParentFont = False
         TabOrder = 5
-        OnDblClick = Pump4GBDblClick
+        OnDblClick = PumpGBDblClick
         object Pump4OnOffCB7: TCheckBox
           Left = 8
           Height = 19
@@ -2760,7 +2760,7 @@ object MainForm: TMainForm
         Enabled = False
         ParentFont = False
         TabOrder = 6
-        OnDblClick = Pump3GBDblClick
+        OnDblClick = PumpGBDblClick
         object Pump3OnOffCB7: TCheckBox
           Left = 8
           Height = 19
@@ -3238,35 +3238,35 @@ object MainForm: TMainForm
   object StepTimer2: TTimer
     Enabled = False
     Interval = 100
-    OnTimer = StepTimer2Finished
+    OnTimer = StepTimerXFinished
     Left = 352
     Top = 392
   end
   object StepTimer3: TTimer
     Enabled = False
     Interval = 100
-    OnTimer = StepTimer3Finished
+    OnTimer = StepTimerXFinished
     Left = 352
     Top = 344
   end
   object StepTimer4: TTimer
     Enabled = False
     Interval = 100
-    OnTimer = StepTimer4Finished
+    OnTimer = StepTimerXFinished
     Left = 352
     Top = 296
   end
   object StepTimer5: TTimer
     Enabled = False
     Interval = 100
-    OnTimer = StepTimer5Finished
+    OnTimer = StepTimerXFinished
     Left = 352
     Top = 248
   end
   object StepTimer6: TTimer
     Enabled = False
     Interval = 100
-    OnTimer = StepTimer6Finished
+    OnTimer = StepTimerXFinished
     Left = 352
     Top = 200
   end
@@ -3283,7 +3283,7 @@ object MainForm: TMainForm
   object StepTimer7: TTimer
     Enabled = False
     Interval = 100
-    OnTimer = StepTimer7Finished
+    OnTimer = StepTimerLastFinished
     Left = 352
     Top = 152
   end

commit 92a0d6b2b4e7cfd5ab0e8482e201bdc19b0bec9f

fix a bug
- endless repeats of several steps could lead to a crash
  (regression introduced by commit 8ea24e4c)

JTPumpDriver2.lpi
@@ -21,8 +21,8 @@
       <UseVersionInfo Value="True"/>
       <MajorVersionNr Value="2"/>
       <MinorVersionNr Value="6"/>
-      <RevisionNr Value="6"/>
-      <StringTable CompanyName="Jobst Technologies" FileDescription="Program to control pump driver for CCP1 pumps" LegalCopyright="Jobst Technologies GmbH" ProductName="JT Pump Driver" ProductVersion="2.66"/>
+      <RevisionNr Value="7"/>
+      <StringTable CompanyName="Jobst Technologies" FileDescription="Program to control pump driver for CCP1 pumps" LegalCopyright="Jobst Technologies GmbH" ProductName="JT Pump Driver" ProductVersion="2.67"/>
     </VersionInfo>
     <BuildModes Count="3">
       <Item1 Name="Default" Default="True"/>

JTPumpDriver2.lps
@@ -20,8 +20,8 @@
         <HasResources Value="True"/>
         <ResourceBaseClass Value="Form"/>
         <IsVisibleTab Value="True"/>
-        <TopLine Value="405"/>
-        <CursorPos X="31" Y="406"/>
+        <TopLine Value="317"/>
+        <CursorPos X="28" Y="1838"/>
         <UsageCount Value="200"/>
         <Loaded Value="True"/>
         <LoadedDesigner Value="True"/>
@@ -46,7 +46,7 @@
         <ResourceBaseClass Value="Form"/>
         <EditorIndex Value="2"/>
         <CursorPos X="40"/>
-        <UsageCount Value="94"/>
+        <UsageCount Value="95"/>
         <Loaded Value="True"/>
       </Unit3>
       <Unit4>
@@ -57,7 +57,7 @@
         <ResourceBaseClass Value="Form"/>
         <EditorIndex Value="-1"/>
         <CursorPos X="31" Y="7"/>
-        <UsageCount Value="66"/>
+        <UsageCount Value="67"/>
       </Unit4>
       <Unit5>
         <Filename Value="..\ICP12A\Lazarus\LazSerial\lazserial.pas"/>
@@ -451,75 +451,75 @@
     <JumpHistory Count="30" HistoryIndex="29">
       <Position1>
         <Filename Value="jtpumpdrivermain2.pas"/>
-        <Caret Line="792" Column="15" TopLine="774"/>
+        <Caret Line="602" Column="43" TopLine="565"/>
       </Position1>
       <Position2>
         <Filename Value="jtpumpdrivermain2.pas"/>
-        <Caret Line="844" Column="15" TopLine="812"/>
+        <Caret Line="791" Column="43" TopLine="774"/>
       </Position2>
       <Position3>
         <Filename Value="jtpumpdrivermain2.pas"/>
-        <Caret Line="45" Column="55" TopLine="29"/>
+        <Caret Line="887" Column="23" TopLine="851"/>
       </Position3>
       <Position4>
         <Filename Value="jtpumpdrivermain2.pas"/>
-        <Caret Line="48" Column="17" TopLine="29"/>
+        <Caret Line="461" Column="13" TopLine="440"/>
       </Position4>
       <Position5>
         <Filename Value="jtpumpdrivermain2.pas"/>
-        <Caret Line="795" Column="40" TopLine="766"/>
+        <Caret Line="463" Column="22" TopLine="440"/>
       </Position5>
       <Position6>
         <Filename Value="jtpumpdrivermain2.pas"/>
-        <Caret Line="571" Column="33" TopLine="562"/>
+        <Caret Line="690" Column="22" TopLine="655"/>
       </Position6>
       <Position7>
         <Filename Value="jtpumpdrivermain2.pas"/>
-        <Caret Line="583" Column="41" TopLine="561"/>
+        <Caret Line="691" Column="23" TopLine="655"/>
       </Position7>
       <Position8>
         <Filename Value="jtpumpdrivermain2.pas"/>
-        <Caret Line="13" Column="27"/>
+        <Caret Line="784" Column="20" TopLine="749"/>
       </Position8>
       <Position9>
         <Filename Value="jtpumpdrivermain2.pas"/>
-        <Caret Line="10" Column="3"/>
+        <Caret Line="785" Column="23" TopLine="749"/>
       </Position9>
       <Position10>
         <Filename Value="jtpumpdrivermain2.pas"/>
-        <Caret Line="323" Column="97" TopLine="313"/>
+        <Caret Line="886" Column="11" TopLine="852"/>
       </Position10>
       <Position11>
         <Filename Value="jtpumpdrivermain2.pas"/>
-        <Caret Line="602" Column="43" TopLine="565"/>
+        <Caret Line="888" Column="23" TopLine="852"/>
       </Position11>
       <Position12>
         <Filename Value="jtpumpdrivermain2.pas"/>
-        <Caret Line="791" Column="43" TopLine="774"/>
+        <Caret Line="461" Column="14" TopLine="442"/>
       </Position12>
       <Position13>
         <Filename Value="jtpumpdrivermain2.pas"/>
-        <Caret Line="887" Column="23" TopLine="851"/>
+        <Caret Line="463" Column="22" TopLine="442"/>
       </Position13>
       <Position14>
         <Filename Value="jtpumpdrivermain2.pas"/>
-        <Caret Line="461" Column="13" TopLine="440"/>
+        <Caret Line="691" Column="23" TopLine="655"/>
       </Position14>
       <Position15>
         <Filename Value="jtpumpdrivermain2.pas"/>
-        <Caret Line="463" Column="22" TopLine="440"/>
+        <Caret Line="785" Column="23" TopLine="749"/>
       </Position15>
       <Position16>
         <Filename Value="jtpumpdrivermain2.pas"/>
-        <Caret Line="690" Column="22" TopLine="655"/>
+        <Caret Line="888" Column="23" TopLine="852"/>
       </Position16>
       <Position17>
         <Filename Value="jtpumpdrivermain2.pas"/>
-        <Caret Line="691" Column="23" TopLine="655"/>
+        <Caret Line="463" Column="22" TopLine="442"/>
       </Position17>
       <Position18>
         <Filename Value="jtpumpdrivermain2.pas"/>
-        <Caret Line="784" Column="20" TopLine="749"/>
+        <Caret Line="691" Column="23" TopLine="655"/>
       </Position18>
       <Position19>
         <Filename Value="jtpumpdrivermain2.pas"/>
@@ -527,47 +527,47 @@
       </Position19>
       <Position20>
         <Filename Value="jtpumpdrivermain2.pas"/>
-        <Caret Line="886" Column="11" TopLine="852"/>
+        <Caret Line="888" Column="23" TopLine="852"/>
       </Position20>
       <Position21>
-        <Filename Value="jtpumpdrivermain2.pas"/>
-        <Caret Line="888" Column="23" TopLine="852"/>
+        <Filename Value="JTPumpDriverMain2.pas"/>
+        <Caret Line="412" Column="32" TopLine="405"/>
       </Position21>
       <Position22>
-        <Filename Value="jtpumpdrivermain2.pas"/>
-        <Caret Line="461" Column="14" TopLine="442"/>
+        <Filename Value="JTPumpDriverMain2.pas"/>
+        <Caret Line="321" Column="27" TopLine="312"/>
       </Position22>
       <Position23>
-        <Filename Value="jtpumpdrivermain2.pas"/>
-        <Caret Line="463" Column="22" TopLine="442"/>
+        <Filename Value="JTPumpDriverMain2.pas"/>
+        <Caret Line="1836" TopLine="1814"/>
       </Position23>
       <Position24>
-        <Filename Value="jtpumpdrivermain2.pas"/>
-        <Caret Line="691" Column="23" TopLine="655"/>
+        <Filename Value="JTPumpDriverMain2.pas"/>
+        <Caret Line="1837" TopLine="1817"/>
       </Position24>
       <Position25>
-        <Filename Value="jtpumpdrivermain2.pas"/>
-        <Caret Line="785" Column="23" TopLine="749"/>
+        <Filename Value="JTPumpDriverMain2.pas"/>
+        <Caret Line="1838" TopLine="1817"/>
       </Position25>
       <Position26>
-        <Filename Value="jtpumpdrivermain2.pas"/>
-        <Caret Line="888" Column="23" TopLine="852"/>
+        <Filename Value="JTPumpDriverMain2.pas"/>
+        <Caret Line="1837" TopLine="1817"/>
       </Position26>
       <Position27>
-        <Filename Value="jtpumpdrivermain2.pas"/>
-        <Caret Line="463" Column="22" TopLine="442"/>
+        <Filename Value="JTPumpDriverMain2.pas"/>
+        <Caret Line="1840" TopLine="1820"/>
       </Position27>
       <Position28>
-        <Filename Value="jtpumpdrivermain2.pas"/>
-        <Caret Line="691" Column="23" TopLine="655"/>
+        <Filename Value="JTPumpDriverMain2.pas"/>
+        <Caret Line="1841" TopLine="1820"/>
       </Position28>
       <Position29>
-        <Filename Value="jtpumpdrivermain2.pas"/>
-        <Caret Line="785" Column="23" TopLine="749"/>
+        <Filename Value="JTPumpDriverMain2.pas"/>
+        <Caret Line="1840" TopLine="1820"/>
       </Position29>
       <Position30>
-        <Filename Value="jtpumpdrivermain2.pas"/>
-        <Caret Line="888" Column="23" TopLine="852"/>
+        <Filename Value="JTPumpDriverMain2.pas"/>
+        <Caret Line="1852" TopLine="1826"/>
       </Position30>
     </JumpHistory>
     <RunParams>

JTPumpDriverMain2.pas
@@ -318,7 +318,7 @@ type
 
 var
   MainForm : TMainForm;
-  Version : string = '2.66';
+  Version : string = '2.67';
   FirmwareVersion : string = 'unknown';
   RequiredFirmwareVersion : float = 1.3;
   ser: TBlockSerial;
@@ -1831,9 +1831,9 @@ var
  SenderName : string;
 begin
  SenderName:= (Sender as TComponent).Name;
- // SenderName is in the form "StepxTS" and we need the x
- // so get the 5th character of the name
- Step:= StrToInt(Copy(SenderName, 5, 1));
+ // SenderName is in the form "StepTimerX" and we need the X
+ // so get the 10th character of the name
+ Step:= StrToInt(Copy(SenderName, 10, 1));
  (FindComponent('StepTimer' + IntToStr(Step))
         as TTimer).Enabled:= False;
  // if there is a step+1, start its timer and show its tab